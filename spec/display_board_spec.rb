d30c07d3c53788e590cbb8b476ac8071b728988c
Meisuleen Kased <meisuleen@gmail.com>
2018-09-30 18:28:05 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -183,7 +183,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # Don't touch the following lines.
     output = capture_puts{ display_board(board) } if defined?(display_board)
     rows = output.split("\n")
-    
+
     expect(rows[0]).to eq(" o | o | o ")
     expect(rows[1]).to eq("-----------")
     expect(rows[2]).to eq(" o | o | o ")
